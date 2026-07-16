a4779d0179126ab5ec668c8f36dd37399e00d569
R1kks <eugene.dozorov.303@gmail.com>
2026-07-17 02:46:58 +0500

    Создание структуры проекта

---
+++ app/src/main.c
@@ -0,0 +1,13 @@
+/*
+main.c - файл, который содержит главную функцию main
+
+Дозоров Владимир Евгеньевич
+Группа: МК - 101
+*/
+
+#include "hex_dumper.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
